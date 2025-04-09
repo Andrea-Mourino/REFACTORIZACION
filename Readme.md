be03aeccc75f1e1b05c3bacf75b717e19f811ccf
Andrea Mourino <amourinodesousa@danielcastelao.org>
2025-04-09 14:21:25 +0200

    extraemos metodo

--- Readme.md
+++ Readme.md
@@ -290,3 +290,4 @@ Podemos seleccionar diferentes elementos para mostrar su men√∫ de refactorizaci√
 
 1. Cambiamos los nombres a los valores para hacermo mas claro
 2. Extraemos un metodo y le cambiamos el nombre a isTin
+3. Hacemos lo mismo para conseguir eol resultado 
--- src/Main.java
+++ src/Main.java
@@ -7,42 +7,18 @@ public class Main {
         int tempScore=0;
 
         if (isTie(pointsPlayer1, pointsPlayer2)) {
-            switch (pointsPlayer1)
-            {
-                case 0:
-                    score = "Love-All";
-                    break;
-                case 1:
-                    score = "Fifteen-All";
-                    break;
-                case 2:
-                    score = "Thirty-All";
-                    break;
-                case 3:
-                    score = "Forty-All";
-                    break;
-                default:
-                    score = "Deuce";
-                    break;
-
-            }
-        }
-        else if (pointsPlayer1 >=4 || pointsPlayer2 >=4)
-        {
-            int minusResult = pointsPlayer1-pointsPlayer2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
-        }
-        else
-        {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = pointsPlayer1;
-                else { score+="-"; tempScore = pointsPlayer2;}
-                switch(tempScore)
-                {
+            score = getTieScores(pointsPlayer1);
+        } else if (isAdvantageOrWin(pointsPlayer1, pointsPlayer2)) {
+            score = getAdvantageOrWinScores(pointsPlayer1, pointsPlayer2);
+        } else {
+            for (int i=1; i<3; i++) {
+                if (i==1)
+                    tempScore = pointsPlayer1;
+                else {
+                    score+="-";
+                    tempScore = pointsPlayer2;
+                }
+                switch(tempScore) {
                     case 0:
                         score+="Love";
                         break;
@@ -60,6 +36,49 @@ public class Main {
 
         }
     return score;
+    }
+
+    private static String getAdvantageOrWinScores(int pointsPlayer1, int pointsPlayer2) {
+        String score;
+        int minusResult = pointsPlayer1 - pointsPlayer2;
+        if (minusResult==1) {
+            score ="Advantage player1";
+        } else if (minusResult ==-1) {
+            score ="Advantage player2";
+        } else if (minusResult>=2) {
+            score = "Win for player1";
+        } else {
+            score ="Win for player2";
+        }
+        return score;
+    }
+
+    private static boolean isAdvantageOrWin(int pointsPlayer1, int pointsPlayer2) {
+        return pointsPlayer1 >= 4 || pointsPlayer2 >= 4;
+    }
+
+    private static String getTieScores(int pointsPlayer1) {
+        String score;
+        switch (pointsPlayer1)
+        {
+            case 0:
+                score = "Love-All";
+                break;
+            case 1:
+                score = "Fifteen-All";
+                break;
+            case 2:
+                score = "Thirty-All";
+                break;
+            case 3:
+                score = "Forty-All";
+                break;
+            default:
+                score = "Deuce";
+                break;
+
+        }
+        return score;
     }
 
     private static boolean isTie(int pointsPlayer1, int pointsPlayer2) {
